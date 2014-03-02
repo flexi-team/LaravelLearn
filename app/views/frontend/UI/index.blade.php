5d630d4b14788b0ae5d0116129d1088ebb001d79
panhna <panhnaseng@yahoo.com>
2014-03-03 00:37:04 +0700

    add iscroll, phototitle and make shaow like paper curls

--- app/views/frontend/UI/index.blade.php
+++ app/views/frontend/UI/index.blade.php
@@ -8,14 +8,17 @@
 
 		<ul class="nav nav-tabs" id="myTab">
 		  	<li class="active"><a href="#Content3D" data-toggle="tab">Flip 3D</a></li>
-		  	<li><a href="#profile" data-toggle="tab">Profile</a></li>
+		  	<li><a href="#slideScroll" data-toggle="tab">Slide & Scroll</a></li>
 		  	<li><a href="#messages" data-toggle="tab">Messages</a></li>
-		  	<li><a href="#settings" data-toggle="tab">Settings</a></li>
+		  	<li><a href="#Other" data-toggle="tab">Other</a></li>
 		</ul>
 
-<!-- 		<div class="tab-content">
-		  	<div class="tab-pane active" id="Content3D">
+		<div class="tab-content">
+
+			<!-- =============== flip 3d content ============= -->
+		  	<div class="tab-pane fade in active" id="Content3D">
 				<div class="row">
+					<a href="http://s3.jt.io/tilt/index.html">Title Photo In Paper Facebook App</a>
 					<h4 class="col-lg-12">3D Switch Content</h4>
 					<div class="col-lg-4 col-lg-offset-4">
 						<div class="box">
@@ -55,7 +58,10 @@
 					<button id="flipNow">Flip Now</button>
 				</div>
 		  	</div>
-		  	<div class="tab-pane" id="profile">
+
+		  	<!-- ================ slide and scroll ============= -->
+		  	<div class="tab-pane fade " id="slideScroll">
+		  		<h4>Owl Plugin</h4>
 				<div id="owl-example" class="owl-carousel">
 				  <div><img src="img/content/product1.jpg" alt="product1"></div>
 				  <div><img src="img/content/product1.jpg" alt="product1"></div>
@@ -66,15 +72,25 @@
 				  <div><img src="img/content/product1.jpg" alt="product1"></div>
 				</div>
 		  	</div>
-		  	<div class="tab-pane" id="messages">
+
+		  	<div class="tab-pane fade " id="messages">
 		  		dfafsf
 		  	</div>
-		  	<div class="tab-pane" id="settings">
-		  		dfadaf
+
+		  	<!-- ================= other style ================= -->
+		  	<div class="tab-pane fade " id="Other">
+		  		<div class="paperCurls">
+		  			<h4>Shadow Paper Curls</h4>
+		  			<div class="box">
+
+		  			</div>
+		  		</div>
 		  	</div>
-		</div> -->
+		</div>
 
 	</div>
+
+	<!-- <input id="test" type="text" value="" placeholder="please input"> -->
 
 	<script type="text/javascript">
 		$(function(){
@@ -83,6 +99,15 @@
 				$('.box-back').toggleClass('flip');
 			})
 			$("#owl-example").owlCarousel();
+			
+			var test=function(){
+				alert('hi');
+			}
+
+			// $("#test").bind("click change blur",function(){
+			// 	test();
+			// })
+
 		})
 	</script>
 
